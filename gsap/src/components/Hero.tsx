9fa8acc80621a76635adf1fd7531a1ca09047838
NunNunIT <hongnhung02v04@gmail.com>
2025-07-03 16:39:46 +0700

    Add Hero component with GSAP animations and video background

---
+++ gsap/src/components/Hero.tsx
@@ -0,0 +1,125 @@
+import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
+
+const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", {
+      type: "words, chars",
+    });
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.05,
+      ease: "expo.out",
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.05,
+      ease: "expo.out",
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: 1,
+        },
+      })
+      .to(".right-leaf", { x: 200 }, 0)
+      .to(".left-leaf", { x: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+    // Video scroll animation timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true, // pin the video during scroll
+      },
+    });
+
+    if (videoRef.current) {
+      videoRef.current.onloadedmetadata = () => {
+        tl.fromTo(
+          videoRef.current,
+          { scale: 0.9 },
+          {
+            currentTime: videoRef.current!.duration,
+            scale: 0.85,
+          }
+        );
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy-">
+        <h1 className="title">NunNun</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right leaf"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crips. Classic</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summner{" "}
+              </p>
+            </div>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails" className="btn">
+                View Cocktails
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          playsInline // no track, no volume
+          preload="auto"
+          muted
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
